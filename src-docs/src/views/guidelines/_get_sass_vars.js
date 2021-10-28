8fe8d7b8501e566326da5872efa0992db7d393fb
farefray <30721331+farefray@users.noreply.github.com>
2021-10-28 17:14:27 +0300

    bugfixes from merge

--- src-docs/src/views/guidelines/_get_sass_vars.js
+++ src-docs/src/views/guidelines/_get_sass_vars.js
@@ -1,13 +1,6 @@
 import saColors from '!!sass-vars-to-js-loader!../../../../../scholarsapp-gcp-spike/shared/ui/variables/_colors.scss';
 
+// SA workaround
 export const useSassVars = () => {
-  const themeContext = useContext(ThemeContext);
-  let palette;
-  switch (theme) {
-    default:
-      palette = saColors;
-      break;
-  }
-
-  return palette;
+  return saColors;
 };
